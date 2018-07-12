7450614864a0ece9d46f5ef156db2ed7ad70fb1b
MICHUDZIK <MICHALCHUDZIK@LIVE.COM>
2018-07-12 15:59:52 +0200

    Add commentator controller

---
+++ app/controllers/commentators_controller.rb
@@ -0,0 +1,35 @@
+class CommentatorsController < ApplicationController
+
+  def index
+    @commentators = Commentator.all
+  end
+
+  def new
+    @commentator = Commentator.new
+  end
+
+  def create
+    @commentator = Commentator.new(commentator_params)
+    if @commentator.save
+      redirect_to commentators_url, notice: 'Commentator added'
+    else
+      redirect_to commentators_url, alert: 'Something went wrong'
+    end
+  end
+
+  def destroy
+    commentator = Commentator.find(params[:id])
+    if commentator.destroy
+      redirect_to commentators_url, notice: 'Commentator deleted' 
+    else
+      redirect_to commentators_url, alert: 'Something went wrong'
+    end
+  end
+
+  private
+
+    def commentator_params
+      params.require(:commentator).permit(:name)
+    end
+
+end
