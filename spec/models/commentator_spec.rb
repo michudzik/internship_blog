4e62393031cbbe166162b09986e7eae2914edc97
MICHUDZIK <MICHALCHUDZIK@LIVE.COM>
2018-07-12 13:13:57 +0200

    Improve commentator's unit tests

--- spec/models/commentator_spec.rb
+++ spec/models/commentator_spec.rb
@@ -2,18 +2,18 @@ require 'rails_helper'
 
 RSpec.describe Commentator, type: :model do
   
-  it 'should have proper attributes' do
-    expect(subject.attributes).to include('name')
+  describe 'attributes' do
+    it 'should have proper attributes' do
+      expect(subject.attributes).to include('name')
+    end
   end
 
-  it 'should have many comments' do
-    t = Commentator.reflect_on_association(:comments)
-    expect(t.macro).to eq(:has_many)
+  describe 'validations' do
+    it { should validate_presence_of(:name) }
   end
 
-  it 'should require name to be present' do
-    expect(Commentator.new).not_to be_valid
-    expect(Commentator.new(name: 'Bob')).to be_valid
+  describe 'relations' do
+    it { should have_many(:comments) }
   end
 
 end
