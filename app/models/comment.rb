daeb7a77a74404f2a9deccba58dbe09245ab85e5
MICHUDZIK <MICHALCHUDZIK@LIVE.COM>
2018-07-12 10:23:04 +0200

    Improve show author view

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,6 +1,6 @@
 class Comment < ActiveRecord::Base
   validates :content, presence: true
-  validates :content, length: { maximum: 160, minimum: 10 }
+  validates :content, length: { maximum: 160, minimum: 8 }
   validates :post_id, presence: true
   validates :commentator_id, presence: true
   
