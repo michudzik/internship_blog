bf943ac47c6248e740c5b73370f8b55605477623
MICHUDZIK <MICHALCHUDZIK@LIVE.COM>
2018-07-12 11:19:49 +0200

    Add allow_nil to age attribute in author

--- app/models/author.rb
+++ app/models/author.rb
@@ -2,7 +2,11 @@ class Author < ActiveRecord::Base
   #has_one :post
   validates :name,      presence:     true
   validates :surname,   presence:     true
-  validates :age,       numericality: { only_integer: true, greater_than: 0 }
+  validates :age,       numericality: { only_integer: true, greater_than: 0, allow_nil: true }
+
+  scope :old, -> { where('age > 30') }
+
+  before_create :default_age
 
   #has_many :author_posts
   #has_many :posts, through: :author_posts, dependent: :destroy
@@ -11,4 +15,11 @@ class Author < ActiveRecord::Base
   def full_name 
     "#{self.name} #{self.surname}"
   end
+
+  private
+
+    def default_age
+      self.age = 25 unless age
+    end
+
 end
