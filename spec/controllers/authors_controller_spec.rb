fcb9fb8b9c2ace400d76f304c0f87c05476d492f
MICHUDZIK <MICHALCHUDZIK@LIVE.COM>
2018-07-14 20:21:53 +0200

    Add update and destroy test's for authors controller

--- spec/controllers/authors_controller_spec.rb
+++ spec/controllers/authors_controller_spec.rb
@@ -100,7 +100,67 @@ RSpec.describe AuthorsController, type: :controller do
       it 'should not change author count' do
         expect{ subject }.not_to change{ Author.count }
       end
-      
+
+    end
+
+  end
+
+  describe '#update' do
+    let(:author) { create(:author) }
+    let(:valid_parameters) { { id: author.id, author: { name: 'valid'} } }
+    let(:invalid_parameters) { { id: author.id, author: { name: nil} } }
+
+    context 'valid params' do
+      subject { patch :update, params: valid_parameters }
+
+      it 'should redirect to authors index' do
+        expect(subject).to redirect_to(authors_url)
+      end
+
+      it 'should redirect with a notice' do
+        subject
+        expect(flash[:notice]).to be_present
+      end
+
+      it 'should change author\'s name' do
+        subject
+        expect(author.reload.name).to eq('valid')
+      end
+
+    end
+
+    context 'invalid params' do
+      subject { patch :update, params: invalid_parameters }
+
+      it 'should render edit template' do
+        expect(subject).to render_template('edit')
+      end
+
+      it 'should not change author\'s name' do
+        subject
+        expect(author.reload.name).not_to eq('valid')
+      end
+
+    end
+
+  end
+
+  describe '#destroy' do
+    let(:author) { create(:author) }
+    subject { delete :destroy, params: { id: author.id } }
+
+    it 'should redirect to authors index' do
+      expect(subject).to redirect_to(authors_url)
+    end
+
+    it 'should redirect with a notice' do
+      subject
+      expect(flash[:notice]).to be_present
+    end
+
+    it 'should change author count' do
+      author_id = author.id
+      expect{ delete :destroy, params: { id: author_id } }.to change{ Author.count }.by(-1)
     end
 
   end
